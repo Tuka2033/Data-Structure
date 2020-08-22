8faee642e0061c60003eda8e05097a2951972c50
Tuka2033 <63064476+Tuka203>
2020-08-21 20:25:17 -0700

    Assignment

--- Aug_21/Aug_21-1/Source.cpp
+++ Aug_21/Aug_21-1/Source.cpp
@@ -11,56 +11,58 @@ public:
 		rear = -1;
 		front = -1;
 		size = 0;
+		arr = NULL;
 	}
 	cDeque(int sz)
 	{
+		rear = -1;
+		front = -1;
 		size = sz;
 		arr = new int[size];
 	}
-	void InsertFront(int data)
+	void insertfront(int data)
 	{
-		if ((front == 0) && (rear = size - 1))
+		if (front == 0 && rear == size - 1)
 		{
-			cout << "\nDequeue is full.....!!!!!!!!";
+			printf("\nDeque is full..\n");
+			return;
 		}
-		else if (front == -1)
+		if (front == -1)
 		{
 			front = rear = 0;
-			arr[rear] = data;
+			arr[front] = data;
+			return;
 		}
-		else if ((rear != size - 1) && (front == 0))
+		if (front == 0 && rear != size - 1)
 		{
-			for (int i = rear; i >= front; i++)
+			for (int i = rear; i >= front; i--)
 			{
 				arr[i + 1] = arr[i];
 			}
 			arr[front] = data;
 			rear++;
 		}
-		else if ((front != 0) && (rear != size - 1))
+		if (front != 0 && rear != size - 1)
 		{
-			front--;
+			front = front - 1;
 			arr[front] = data;
-        }
+		}
 	}
 
-	void InsertRear(int data)
+	void insertrear(int data)
 	{
-		if ((rear == size) && (front == 0))
+		if (front == 0 && rear == size - 1)
 		{
-			cout << "\nDeque is full....!!!!!";
+			printf("\nDeque is full\n");
+			return;
 		}
-		else if (front == -1)
+		if (front == -1)
 		{
-			rear = front = 0;
+			front = rear = 0;
 			arr[rear] = data;
+			return;
 		}
-		else if ((front == 0) && (rear <= size-1))
-		{
-			arr[rear] = data;
-			rear++;
-		}
-		else if ((front > 0) && (rear == size-1))
+		if (rear == size - 1)
 		{
 			for (int i = front; i <= rear; i++)
 			{
@@ -68,78 +70,102 @@ public:
 			}
 			arr[rear] = data;
 			front--;
-			rear++;
-		}
-	}
-	void deleteFront()
-	{
-		if (front == -1)
-		{
-			cout << "\nDequeue is empty....!!!";
-		}
-		else if(front!=rear)
-		{
-			int ele = arr[front];
-			cout << "\nDeleted Element is::"<<ele;
-			front++;
-		}
-		else if (rear == front)
-		{
-			int ele = arr[front];
-			cout << "\nDeleted Element is::" << ele;
-			front=rear=-1;
-		}
-	}
-	void deleteRear()
-	{
-		if (front == -1)
-		{
-			cout << "\nDequeue is empty....!!!";
-		}
-		else if (rear == front)
-		{
-			int ele = arr[rear];
-			cout << "\nDeleted Element is::" << ele;
-			front = rear = -1;
 		}
 		else
 		{
-			int ele = arr[rear];
-			cout << "\nDeleted Element is::" << ele;
-			rear--;
+			rear++;
+			arr[rear] = data;
+		}
+	}
+	void deletefront()
+	{
+		int item;
+		if (front == -1)
+		{
+			printf("\ndeque is empty..\n");
+		}
+		else
+		{
+			item = arr[front];
+			printf("\ndeleted element is::", item);
+			arr[front] = 0;
+			if (front == rear)
+			{
+				front = rear = -1;
+			}
+			else
+			{
+				front++;
+			}
+		}
+	}
+	void deleterear()
+	{
+		int item;
+		if (front == -1)
+		{
+			printf("\ndeque is empty..\n");
+		}
+		else
+		{
+			item = arr[rear];
+			printf("\ndeleted element is::", item);
+			arr[rear] = 0;
+			if (front == rear)
+			{
+				front = rear = -1;
+			}
+			else
+			{
+				rear--;
+			}
 		}
 	}
 	void Display()
 	{
 		cout << "\nFront-->";
-		for (int i = front; i < rear; i++)
+		for (int i = front; i <= rear; i++)
 		{
-			cout << arr[i]<<" ";
+	    	cout << arr[i] << " ";
 		}
 	}
 };
 int main()
 {
-	cDeque d(9);
-	//
-	d.InsertRear(10);
-	d.InsertRear(20);
-	d.InsertRear(30);
-	d.InsertRear(40);
-	d.InsertRear(50);
-	d.InsertRear(60);
-	d.InsertRear(70);
-	d.InsertRear(80);
-	d.InsertRear(90);
-	d.Display();
-	d.deleteFront();
-	d.Display();
-	d.InsertFront(100);
-	d.Display();
-	d.deleteFront();
-	d.Display();
-	d.InsertRear(110);
-	d.InsertRear(120);
-	d.deleteRear();
-	d.Display();
+	cDeque d(6);
+	int choice, ele;
+	do
+	{
+		printf("\n1.Insert front\n");
+		printf("2.Insert rear\n");
+		printf("3.delete front\n");
+		printf("4.delete rear\n");
+		printf("5.exit\n");
+		cout<<"Enter ur choice::";
+		cin>>choice;
+		switch (choice)
+		{
+		case 1:
+			cout<<"\nEnter an element to be insert ::";
+			cin >> ele;
+			d.insertfront(ele);
+			d.Display();
+			break;
+		case 2:
+			cout << "\nEnter an element to be insert ::";
+			cin >> ele;
+			d.insertrear(ele);
+			d.Display();
+			break;
+		case 3:
+			d.deletefront();
+			d.Display();
+			break;
+		case 4:
+			d.deleterear();
+			d.Display();
+			break;
+		}
+	} while (choice != 5);
+	return 0;
 }
